1a1c78b8a334b2db4c598cad7d5c842c72184302
zhenglier <3080529100@qq.com>
2024-12-01 21:32:31 +0800

    change

---
+++ build/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/src/main.cpp.o"
+  "CMakeFiles/compiler.dir/src/main.cpp.o.d"
+  "CMakeFiles/compiler.dir/src/parser/SyntaxAnalyse.cpp.o"
+  "CMakeFiles/compiler.dir/src/parser/SyntaxAnalyse.cpp.o.d"
+  "CMakeFiles/compiler.dir/src/parser/SyntaxTree.cpp.o"
+  "CMakeFiles/compiler.dir/src/parser/SyntaxTree.cpp.o.d"
+  "CMakeFiles/compiler.dir/src/parser/lexer.cpp.o"
+  "CMakeFiles/compiler.dir/src/parser/lexer.cpp.o.d"
+  "CMakeFiles/compiler.dir/src/parser/parser.cpp.o"
+  "CMakeFiles/compiler.dir/src/parser/parser.cpp.o.d"
+  "compiler"
+  "compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
